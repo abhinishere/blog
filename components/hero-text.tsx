2008e6200c3fa3dc7d772e03235442475236089a
Abhin Mahipal <abhinishere@gmail.com>
2023-07-29 14:31:45 +0530

    v1.2 - bug fixes + fav

--- components/hero-text.tsx
+++ components/hero-text.tsx
@@ -5,10 +5,10 @@ export default function HeroText() {
   return (
     <div className="ml:space-y-10 space-y-14">
       <p className="text-justify">
-        👋 Hi, I'm Abhin. Freelance developer and writer. I'm passionate about
-        building web experiences that solve real problems and improve lives.
-        When not working, I am mostly likely digging through Reddit or spinning
-        up websites around obscure topics.
+        👋 Hi, I&apos;m Abhin. Freelance developer and writer. I&apos;m
+        passionate about building web experiences that solve real problems and
+        improve lives. When not working, I am mostly likely digging through
+        Reddit or spinning up websites around obscure topics.
       </p>
       <div className="flex flex-wrap items-center sm:space-x-2">
         {socialLinks.map((link) => (
